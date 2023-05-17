f2fa38c13a2acdd38e1e1f032de6d1d860753938
Francesca Santoriello <f.santoriello@hotmail.com>
2023-05-17 11:36:23 +0200

    comment test

--- bookbeforeui/src/App.js
+++ bookbeforeui/src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
-import axios from 'axios';
-import Dishes from './components/dishes';
-import { useState, useEffect } from 'react';
 
-const API_URL = 'http://localhost:3001/api/v1/dishes';
+// import axios from 'axios';
+// import Dishes from './components/dishes';
+// import { useState, useEffect } from 'react';
 
-function getAPIData() {
-    return axios.get(API_URL)
-    .then((response) => response.data);
-}
+// const API_URL = 'http://localhost:3001/api/v1/dishes';
+
+// function getAPIData() {
+//     return axios.get(API_URL)
+//     .then((response) => response.data);
+// }
 
 function App() {
-    const [dishes, setDishes] = useState([]);
-
-    useEffect(() => {
-        let mounted = true;
-        getAPIData().then((items) => {
-            if (mounted) {
-                setDishes(items);
-            }
-        });
-        return () => (mounted = false);
-    }, []);
-
-    return (
-        <div className="App">
-            <h1>Dishes</h1>
-            <Dishes dishes={dishes} />
-        </div>
-    );
 }
 
 export default App;
